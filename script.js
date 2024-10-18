88c4f47f497bc054281cf98ccf38556c4ba875a8
Pikin <pikin@bk.ru>
2024-10-18 17:30:38 +0500

    events added

--- script.js
+++ script.js
@@ -8,19 +8,21 @@ const imageContainer = [
 let index = 0;
 const pointsContainer = document.querySelector('.points');
 const numberOfPoints = imageContainer.length;
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const points = document.querySelector('.points');
 for (let i = 0; i < numberOfPoints; i++) {
     const point = document.createElement('div');
     point.classList.add('circle');
     point.setAttribute('index', i);
     pointsContainer.appendChild(point);
+    addBorder();
 }
 
 const currentImg = document.querySelector('.image');
 currentImg.src = imageContainer[index];
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const points = document.querySelector('.points');
+
 
 prevBtn.addEventListener('click', () =>{
     if (index > 0) {
@@ -29,7 +31,7 @@ prevBtn.addEventListener('click', () =>{
         index = numberOfPoints-1;
         currentImg.src = imageContainer[index];
     }
-    
+    addBorder();
 });
 
 nextBtn.addEventListener('click', () =>{
@@ -39,12 +41,27 @@ nextBtn.addEventListener('click', () =>{
         index = 0;
         currentImg.src = imageContainer[index];
     }
-    
+    addBorder();
 });
 
 points.addEventListener('click', (e) =>{
-    index = e.target.getAttribute('index')
+    index = Number(e.target.getAttribute('index'))
     currentImg.src = imageContainer[index];
+    addBorder();
+    
 });
+
+function addBorder() {
+    const pointsArray = Array.from(points.children);
+    pointsArray.forEach(point =>{
+
+        if (index === Number(point.getAttribute('index'))) {
+                point.classList.add('change-color');
+        } else {
+                    console.log('no')
+                    point.classList.remove('change-color');
+                }
+    })  
+}
 
 
